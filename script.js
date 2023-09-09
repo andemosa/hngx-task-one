c453bf01e9b1913369d40ddacd51fd5af78291f2
andemosa <osayerieanderson@yahoo.com>
2023-09-09 18:45:34 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const dayElem = document.getElementById("currentDay");
+const timeElem = document.getElementById("currentTime");
+
+function displayDateTime() {
+  const currentDate = new Date();
+
+  const dayOfWeek = currentDate.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+
+  const currentTimeInMilliseconds = Date.now();
+
+  dayElem.textContent = `Current Day of the week: ${dayOfWeek}`;
+
+  timeElem.textContent = `Current UTC Time: ${currentTimeInMilliseconds}`;
+}
+
+setInterval(displayDateTime, 1000);
+
+displayDateTime();
